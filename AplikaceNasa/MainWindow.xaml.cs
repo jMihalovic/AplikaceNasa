2f4a476b54c7424030fe05676f6766f1e7b46381
Jakub Mihalovič <mihalovic.jakub.2018@skola.ssps.cz>
2021-12-20 19:31:20 +0100

    h

--- AplikaceNasa/MainWindow.xaml.cs
+++ AplikaceNasa/MainWindow.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace AplikaceNasa
 {
@@ -22,28 +26,83 @@ namespace AplikaceNasa
     /// </summary>
     public partial class MainWindow : Window
     {
-        static HttpClient client = new HttpClient();
+
+        static Rootobject AsteroidData;
+
+        static Rootobject OfflineAsteroids;
+
+        static API_Data info;
+
+        public static ObservableCollection<Asteroid> AsteroidsOnline { get; set; }
+        public static ObservableCollection<Asteroid> AsteroidFavs { get; set; }
+        public static ObservableCollection<Asteroid> AsteroidsAll { get; set; }
+
         public MainWindow()
+        {
+            InitializeComponent();
+            ZiskejOnlineData();
+            while(info == null)
+            {
+
+
+
+            }
+
+            string s;
+            StreamReader sr = new StreamReader("Json.txt");
+            s = sr.ReadToEnd();
+            if (s != null && s != "")
+            {
+                OfflineAsteroids = JsonConvert.DeserializeObject<Rootobject>(s);
+                AsteroidFavs = new ObservableCollection<Asteroid>(OfflineAsteroids.near_earth_objects.asteroids);
+            }
+            try
+            {
+                foreach (var x in AsteroidsOnline)
+                {
+
+                    AsteroidsAll.Add(x);
+
+                }
+            }
+            catch { }
+            try
+            {
+                foreach (var x in AsteroidFavs)
+                {
+
+                    AsteroidsAll.Add(x);
+
+                }
+                
+            }
+            catch { }
+            Asteroid_List.ItemsSource = AsteroidsAll;
+        }
+
+        private async void ZiskejOnlineData()
         {
             string API_KEY = "6s4paFhcy981wiKvqPT2Awc1StRsc2ubhJsgE8pu";
             DateTime now = DateTime.Now;
             string odkaz = $"https://api.nasa.gov/neo/rest/v1/feed?start_date={now.Year}-{now.Month}-{now.Day}&end_date={now.Year}-{now.Month}-{now.Day}&api_key={API_KEY}";
 
-            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() == true)               
-            { 
-            
-
-
-            }
-            else 
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() == true)
             {
-            
-            
+
+                info = await Volani_API.Get(odkaz);
+
+                if (info.Data != null && info.Data != "")
+                {
+                    Regex reg = new Regex(@"\d{4}[-]\d{2}[-]\d{2}");
+                    string database = reg.Replace(info.Data, "date", 1, 500);
+
+                    AsteroidData = JsonConvert.DeserializeObject<Rootobject>(database);
+                    AsteroidsOnline = new ObservableCollection<Asteroid>(AsteroidData.near_earth_objects.asteroids);
+                }
 
             }
 
 
-            InitializeComponent();
         }
 
         private void Asteroids_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -51,7 +110,7 @@ namespace AplikaceNasa
 
         }
 
-        private void Asteroids_MouseDoubleClick(object sender, SelectionChangedEventArgs e)
+        private void Asteroid_List_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             Informace inf = new Informace();
             inf.ShowDialog();
---
+++ AplikaceNasa/Volani_API.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AplikaceNasa
+{
+    public class Volani_API
+    {
+        public static async Task<API_Data> 
+            Get(string odkaz, string auth = null)
+        {
+            using (var http = new HttpClient())
+            {
+
+                var request = await http.GetAsync(odkaz);
+
+                if (request.IsSuccessStatusCode)
+                {
+                    return new API_Data { Data = await request.Content.ReadAsStringAsync() };
+                }
+                else
+                    return null;
+            }
+        }
+    }
+
+    public class API_Data
+    {
+        public string Data { get; set; }
+    }
+}
